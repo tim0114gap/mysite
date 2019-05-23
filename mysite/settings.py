2593561603cd002c3ef71ddf573e3279aca6fc99
tim0114gap <tim.0114.gap@gmail.com>
2019-05-23 20:02:48 +0900

    heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -146,10 +146,10 @@ STATICFILES_DIRS = (
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-#SECRET_KEY = config('SECRET_KEY')
-#DEBUG = config('DEBUG', default=False, cast=bool)
-#DATABASES = {
- #           'default': dj_database_url.config(
-  #                      default=config('DATABASE_URL')
-   #                         )
-    #        }
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+DATABASES = {
+            'default': dj_database_url.config(
+            default=config('DATABASE_URL')
+            )
+        }
