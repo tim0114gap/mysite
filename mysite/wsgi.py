819c92ac7c04429bd1b04bd1369a676d1180a696
tim0114gap <tim.0114.gap@gmail.com>
2019-05-24 22:25:48 +0900

    heroku

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -7,11 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
-import os
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# import os
+# from django.core.wsgi import get_wsgi_application
+# from whitenoise.django import DjangoWhiteNoise
+#
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+#
+# application = get_wsgi_application()
+# application = DjangoWhiteNoise(application)
