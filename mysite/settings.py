6b8927cbbd6f429d9c1b29da1c2096b54ef57911
tim0114gap <tim.0114.gap@gmail.com>
2019-05-27 23:37:11 +0900

    heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'sykb2)16%sr03yognc)%8a^gp0q)3h&+3dm7w6k8_svx@(vc+z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
+DEBUG = True
 #DEBUG = config('DEBUG', cast=bool)
 ALLOWED_HOSTS = '*'
 
