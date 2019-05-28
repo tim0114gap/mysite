3817156586511cff8711db0a37d6681c4c956168
tim0114gap <tim.0114.gap@gmail.com>
2019-05-28 22:29:36 +0900

    heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -164,7 +164,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, '/blog/static/')
 
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
