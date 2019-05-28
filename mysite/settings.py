dd9963098d3ffb21f57387eeb1054f73827f9455
tim0114gap <tim.0114.gap@gmail.com>
2019-05-28 20:34:02 +0900

    heroku"

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = 'sykb2)16%sr03yognc)%8a^gp0q)3h&+3dm7w6k8_svx@(vc+z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
+SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = '*'
 
