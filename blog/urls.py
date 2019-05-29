528f11920b736ab0e4e84fc97b920d0fedfdfd41
tim0114gap <tim.0114.gap@gmail.com>
2019-05-29 14:32:20 +0900

    url changed

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
         path('' ,views.index ,name='index'),
-        path('work/' ,views.work ,name='work'),
+        path('portfolio/' ,views.work ,name='portfolio'),
         path('profile/' ,views.profile ,name='profile'),
         path('contact/' ,views.contact ,name='contact'),
         path('blog/' ,views.blog ,name='blog')
